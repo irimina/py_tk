4b3bde21c3de5dc2220962ed58ac0c339d52c9d7
irimina <irimina@gmail.com>
2017-06-27 18:29:42 -0400

    Add files via upload

---
+++ animation_parallel_tk.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import time
+tk =Tk()
+canvas= Canvas(tk,width=400,height=400)
+canvas.pack()
+canvas.create_polygon(10,10,10,60,50,35)              
+canvas.create_text(100,100, text='This is a lovely text', fill='red')
+canvas.create_polygon(10,10,10,60,50,35)
+
+for x in range(0,60):
+     canvas.move(1,5,0)# object 1, 5 pixels across, 0 pixels down
+     canvas.move(2,3,0) # object 2(text), 3 pixels across, 0 pixels down
+     canvas.move(3,5,5) # object 3, 5 pixels across, 5 pixels down
+     tk.update()
+     time.sleep(0.05)
+
+for x in range(0,60):
+     canvas.move(1,-5,0)
+     canvas.move(2,-3,0)
+     canvas.move(3,-5,-5)
+     tk.update()
+     time.sleep(0.01)
+
+'''    
+for x in range(0,60):
+     canvas.move(2,3,0)
+     tk.update()
+     time.sleep(0.05)
+
+     
+for x in range(0,60):
+     canvas.move(3,5,5)
+     tk.update()
+     
+ '''    
